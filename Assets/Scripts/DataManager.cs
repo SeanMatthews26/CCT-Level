55e230d117327083cc93b7d84fa52c994621bb00
Sean <Sean3.matthews@live.uwe.ac.uk>
2023-11-29 13:36:14 +0000

    update

--- Assets/Scripts/DataManager.cs
+++ Assets/Scripts/DataManager.cs
@@ -6,8 +6,17 @@ using UnityEngine.UI;
 
 public class DataManager : MonoBehaviour
 {
+    //Current Navigational Tool
+    enum Tool {miniMap, compass, none};
+    [SerializeField] Tool currentTool;
+
+    //MiniMap
     [SerializeField] private RawImage minimap;
     private RectTransform miniMapRT;
+
+    //Timer
+    private float totalPlayTime = 0f;
+    private float timeLookingAtTool = 0f;
 
     // Start is called before the first frame update
     void Start()
@@ -24,11 +33,24 @@ public class DataManager : MonoBehaviour
             if (RectTransformUtility.RectangleContainsScreenPoint(miniMapRT, gazePoint.Screen))
             {
                 minimap.color = Color.red;
+                timeLookingAtTool += Time.deltaTime;
             }
             else
             {
                 minimap.color = Color.white;
             }
         }
+
+        //Timer
+        totalPlayTime += Time.deltaTime;
+    }
+
+    private void OnApplicationQuit()
+    {
+        float percentTimeLookingAtTool = (timeLookingAtTool / totalPlayTime) * 100;
+
+        Debug.Log(totalPlayTime);
+        Debug.Log(timeLookingAtTool);
+        Debug.Log(percentTimeLookingAtTool);
     }
 }
